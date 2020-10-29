13d4bf92a573a7e9c3351b05d2da851135b2f169
snicmakino <snicmakino@gmail.com>
2020-10-30 06:22:56 +0900

    refactor: ファイル分割（StatementとStatemanetData）

--- src/main/kotlin/Statement.kt
+++ src/main/kotlin/Statement.kt
@@ -1,7 +1,5 @@
 import json.Invoice
-import json.Performance
 import json.Play
-import kotlin.math.floor
 
 
 fun statement(invoice: Invoice, plays: Map<String, Play>): String {
@@ -41,77 +39,4 @@ fun renderHtml(data: StatementData): String {
 
 fun usd(amount: Int): String {
     return "\$%.2f".format(amount.toFloat())
-}
-
-class StatementData(
-    _customer: String,
-    _performances: List<Performance>,
-    plays: Map<String, Play>
-) {
-    val customer: String = _customer
-    val performances: List<StatementPerformance>
-    val totalAmount: Int
-    val totalVolumeCredits: Double
-
-    init {
-        fun playFor(it: Performance): Play {
-            return plays[it.playId] ?: error("playId not found")
-        }
-
-        fun amountFor(aPerformance: Performance, play: Play): Int {
-            var result: Int
-            when (play.type) {
-                "tragedy" -> {
-                    result = 40000
-                    if (aPerformance.audience > 30) {
-                        result += 1000 * (aPerformance.audience - 30)
-                    }
-                }
-                "comedy" -> {
-                    result = 30000
-                    if (aPerformance.audience > 20) {
-                        result += 10000 + 500 * (aPerformance.audience - 20)
-                    }
-                    result += 300 * aPerformance.audience
-                }
-                else -> {
-                    throw Exception("unknown type: %s".format(play.type))
-                }
-            }
-            return result / 100
-        }
-
-        fun volumeCreditsFor(aPerformance: Performance, play: Play): Double {
-            var result = 0.0
-            result += listOf(aPerformance.audience - 30, 0).maxOrNull() ?: 0
-            if ("comedy" == play.type) result += floor(aPerformance.audience.toDouble()) / 5
-            return result
-        }
-
-        fun totalAmount(performances: List<StatementPerformance>): Int {
-            return performances.fold(0) { acc, performance -> acc + performance.amount }
-        }
-
-        fun totalVolumeCredits(performances: List<StatementPerformance>): Double {
-            return performances.fold(0.0) { acc, performance -> acc + performance.volumeCredits }
-        }
-
-        performances = _performances.map {
-            StatementPerformance(
-                playFor(it),
-                it.audience,
-                amountFor(it, playFor(it)),
-                volumeCreditsFor(it, playFor(it))
-            )
-        }
-        totalAmount = totalAmount(performances)
-        totalVolumeCredits = totalVolumeCredits(performances)
-    }
-
-    class StatementPerformance(
-        val play: Play,
-        val audience: Int,
-        val amount: Int,
-        val volumeCredits: Double
-    )
 }
---
+++ src/main/kotlin/StatementData.kt
@@ -0,0 +1,76 @@
+import json.Performance
+import json.Play
+import kotlin.math.floor
+
+class StatementData(
+    _customer: String,
+    _performances: List<Performance>,
+    plays: Map<String, Play>
+) {
+    val customer: String = _customer
+    val performances: List<PerformanceData>
+    val totalAmount: Int
+    val totalVolumeCredits: Double
+
+    init {
+        fun playFor(it: Performance): Play {
+            return plays[it.playId] ?: error("playId not found")
+        }
+
+        fun amountFor(aPerformance: Performance, play: Play): Int {
+            var result: Int
+            when (play.type) {
+                "tragedy" -> {
+                    result = 40000
+                    if (aPerformance.audience > 30) {
+                        result += 1000 * (aPerformance.audience - 30)
+                    }
+                }
+                "comedy" -> {
+                    result = 30000
+                    if (aPerformance.audience > 20) {
+                        result += 10000 + 500 * (aPerformance.audience - 20)
+                    }
+                    result += 300 * aPerformance.audience
+                }
+                else -> {
+                    throw Exception("unknown type: %s".format(play.type))
+                }
+            }
+            return result / 100
+        }
+
+        fun volumeCreditsFor(aPerformance: Performance, play: Play): Double {
+            var result = 0.0
+            result += listOf(aPerformance.audience - 30, 0).maxOrNull() ?: 0
+            if ("comedy" == play.type) result += floor(aPerformance.audience.toDouble()) / 5
+            return result
+        }
+
+        fun totalAmount(performances: List<PerformanceData>): Int {
+            return performances.fold(0) { acc, performance -> acc + performance.amount }
+        }
+
+        fun totalVolumeCredits(performances: List<PerformanceData>): Double {
+            return performances.fold(0.0) { acc, performance -> acc + performance.volumeCredits }
+        }
+
+        performances = _performances.map {
+            PerformanceData(
+                playFor(it),
+                it.audience,
+                amountFor(it, playFor(it)),
+                volumeCreditsFor(it, playFor(it))
+            )
+        }
+        totalAmount = totalAmount(performances)
+        totalVolumeCredits = totalVolumeCredits(performances)
+    }
+}
+
+class PerformanceData(
+    val play: Play,
+    val audience: Int,
+    val amount: Int,
+    val volumeCredits: Double
+)
