3bfd1790fcb7689e856c2aee57945668c320dec2
snicmakino <snicmakino@gmail.com>
2020-10-30 05:50:29 +0900

    refactor: 合計値出力の関数化

--- src/main/kotlin/Statement.kt
+++ src/main/kotlin/Statement.kt
@@ -5,8 +5,7 @@ import kotlin.math.floor
 
 
 fun statement(invoice: Invoice, plays: Map<String, Play>): String {
-    val statementData = StatementData(invoice.customer, invoice.performances, plays)
-    return renderPlainText(statementData)
+    return renderPlainText(StatementData(invoice.customer, invoice.performances, plays))
 }
 
 fun renderPlainText(data: StatementData): String {
@@ -33,52 +32,43 @@ class StatementData(
     val totalAmount: Int
     val totalVolumeCredits: Double
 
-    private val playFor: (Performance) -> Play = { plays[it.playId] ?: error("playId not found") }
-    private val amountFor: (Performance, Play) -> Int = { aPerformance, play ->
-        var result: Int
-        when (play.type) {
-            "tragedy" -> {
-                result = 40000
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30)
-                }
-            }
-            "comedy" -> {
-                result = 30000
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20)
-                }
-                result += 300 * aPerformance.audience
-            }
-            else -> {
-                throw Exception("unknown type: %s".format(play.type))
-            }
-        }
-        result
-    }
-    private val volumeCreditsFor: (Performance, Play) -> Double = { aPerformance, play ->
-        var result = 0.0
-        result += listOf(aPerformance.audience - 30, 0).maxOrNull() ?: 0
-        if ("comedy" == play.type) result += floor(aPerformance.audience.toDouble()) / 5
-        result
-    }
-    private val _totalAmount: (List<StatementPerformance>) -> Int = { performances ->
-        var result = 0
-        for (perf in performances) {
-            // 注文の内訳を出力
-            result += perf.amount
-        }
-        result
-    }
-    private val _totalVolumeCredits: (List<StatementPerformance>) -> Double = { performances ->
-        var result = 0.0
-        for (perf in performances) {
-            result += perf.volumeCredits
-        }
-        result
-    }
-
     init {
+        val playFor: (Performance) -> Play = { plays[it.playId] ?: error("playId not found") }
+        val amountFor: (Performance, Play) -> Int = { aPerformance, play ->
+            var result: Int
+            when (play.type) {
+                "tragedy" -> {
+                    result = 40000
+                    if (aPerformance.audience > 30) {
+                        result += 1000 * (aPerformance.audience - 30)
+                    }
+                }
+                "comedy" -> {
+                    result = 30000
+                    if (aPerformance.audience > 20) {
+                        result += 10000 + 500 * (aPerformance.audience - 20)
+                    }
+                    result += 300 * aPerformance.audience
+                }
+                else -> {
+                    throw Exception("unknown type: %s".format(play.type))
+                }
+            }
+            result
+        }
+        val volumeCreditsFor: (Performance, Play) -> Double = { aPerformance, play ->
+            var result = 0.0
+            result += listOf(aPerformance.audience - 30, 0).maxOrNull() ?: 0
+            if ("comedy" == play.type) result += floor(aPerformance.audience.toDouble()) / 5
+            result
+        }
+        val _totalAmount: (List<StatementPerformance>) -> Int = { performances ->
+            performances.fold(0) { acc, performance -> acc + performance.amount }
+        }
+        val _totalVolumeCredits: (List<StatementPerformance>) -> Double = { performances ->
+            performances.fold(0.0) { acc, performance -> acc + performance.volumeCredits }
+        }
+
         performances = _performances.map {
             StatementPerformance(
                 playFor(it),
