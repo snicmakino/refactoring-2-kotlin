e9d1bb897cfc7ae3bfa63098d57565cb5458942b
snicmakino <snicmakino@gmail.com>
2020-11-04 05:43:27 +0900

    refactor: amountForをcalculatorに移動(P37)

--- src/main/kotlin/PerformanceCalculator.kt
+++ src/main/kotlin/PerformanceCalculator.kt
@@ -1,4 +1,28 @@
 import json.Performance
 import json.Play
 
-class PerformanceCalculator(val aPerformance: Performance, val play: Play)
+class PerformanceCalculator(val aPerformance: Performance, val play: Play) {
+
+    fun amount(): Int {
+        var result: Int
+        when (play.type) {
+            "tragedy" -> {
+                result = 40000
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30)
+                }
+            }
+            "comedy" -> {
+                result = 30000
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20)
+                }
+                result += 300 * aPerformance.audience
+            }
+            else -> {
+                throw Exception("unknown type: %s".format(play.type))
+            }
+        }
+        return result / 100
+    }
+}
--- src/main/kotlin/StatementData.kt
+++ src/main/kotlin/StatementData.kt
@@ -17,29 +17,6 @@ class StatementData(
             return plays[it.playId] ?: error("playId not found")
         }
 
-        fun amountFor(aPerformance: Performance, play: Play): Int {
-            var result: Int
-            when (play.type) {
-                "tragedy" -> {
-                    result = 40000
-                    if (aPerformance.audience > 30) {
-                        result += 1000 * (aPerformance.audience - 30)
-                    }
-                }
-                "comedy" -> {
-                    result = 30000
-                    if (aPerformance.audience > 20) {
-                        result += 10000 + 500 * (aPerformance.audience - 20)
-                    }
-                    result += 300 * aPerformance.audience
-                }
-                else -> {
-                    throw Exception("unknown type: %s".format(play.type))
-                }
-            }
-            return result / 100
-        }
-
         fun volumeCreditsFor(aPerformance: Performance, play: Play): Double {
             var result = 0.0
             result += listOf(aPerformance.audience - 30, 0).maxOrNull() ?: 0
@@ -60,7 +37,7 @@ class StatementData(
             PerformanceData(
                 calculator.play,
                 it.audience,
-                amountFor(it, playFor(it)),
+                calculator.amount(),
                 volumeCreditsFor(it, playFor(it))
             )
         }
